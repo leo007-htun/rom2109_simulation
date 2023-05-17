05c769b7056cb3019eb1bf1d2d0854d90f57068b
PyaeSoanAung <romrobotics@gmail.com>
2023-05-17 19:47:05 +0630

    controller manager OK

--- rom2109_description/launch/description_ros2_control.launch.py
+++ rom2109_description/launch/description_ros2_control.launch.py
@@ -7,20 +7,28 @@ from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 
+import xacro
+
 def generate_launch_description():
     # gazebo_pkg = get_package_share_directory('rom2109_gazebo')
     # gz_ros_pkg = get_package_share_directory('gazebo_ros')
     use_sim_time = LaunchConfiguration('use_sim_time')
     
     urdf_pkg = get_package_share_directory('rom2109_description')
-    urdf_path= os.path.join(urdf_pkg, 'urdf/raw', "rom2109_tall.urdf")
-    urdf = open(urdf_path).read()
+    #urdf_path= os.path.join(urdf_pkg, 'urdf', "urdf.urdf")
+    #urdf = open(urdf_path).read()
+    xacro_file = os.path.join(urdf_pkg,'urdf', 'robot_sim_ros2_control.urdf.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+
+    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
 
     robot_state_publisher_node = Node(
         name="robot_state_publisher",
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        parameters=[{"robot_description": urdf, 'use_sim_time': use_sim_time}],
+        output='screen',
+        parameters=[params]
+        #parameters=[{"robot_description": urdf, 'use_sim_time': use_sim_time}],
     )
 
     # joint_state_node = Node(
@@ -38,7 +46,7 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
-            DeclareLaunchArgument('open_rviz', default_value='true', description='Open RViz.'),
+            DeclareLaunchArgument('use_sim_time', default_value='true', description='Use sim time if true'),
             robot_state_publisher_node,
             #joint_state_node,
             #rviz_node
