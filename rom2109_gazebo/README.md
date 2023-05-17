1bb33011efa3e8302794f449d7dc2fe94f38948a
PyaeSoanAung <romrobotics@gmail.com>
2023-05-17 22:06:57 +0630

    gz ros2 control ok

--- rom2109_gazebo/README.md
+++ rom2109_gazebo/README.md
@@ -1,3 +1,5 @@
 ### rom2109_gazebo
 
 models များကို <a href="www.google.com"> ဒီမှာ </a> ဒေါင်းပါ။
+
+#### 
--- rom2109_gazebo/launch/test_rom2109_sim_ros2_control_delayed.launch.py
+++ rom2109_gazebo/launch/test_rom2109_sim_ros2_control_delayed.launch.py
@@ -3,25 +3,20 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, TimerAction, RegisterEventHandler
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
-import xacro
+from launch.event_handlers import OnProcessExit, OnProcessStart
 
 def generate_launch_description():
     gazebo_pkg = get_package_share_directory('rom2109_gazebo')
     description_pkg = get_package_share_directory('rom2109_description')
     default_world_path = os.path.join(gazebo_pkg, 'worlds', 'cafe.world')
 
-    urdf_file = os.path.join(description_pkg,'urdf', 'urdf.urdf')
-    # robot_description_config = xacro.process_file(xacro_file)
-    # my_xml = robot_description_config.toxml()
-
     bot = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            description_pkg, 'launch', 'description_ros2_control.launch.py'
+            description_pkg, 'launch', 'description_xacro_sim_ros2_control.launch.py'
         )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
@@ -51,27 +46,40 @@ def generate_launch_description():
             "extra_gazebo_args": "--ros-args --params-file " + gazebo_params_file
         }.items(),
     )
-
+    
     spawn_robot_node = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        arguments=['-file', urdf_file, '-entity', 'rom2109',
+        arguments=['-topic', 'robot_description', '-entity', 'rom2109',
                    "-x", '0.0',
                    "-y", '0.0',
                    "-z", '0.1'],
         output='screen'
     )
+    delayed_spawn_robot_node = TimerAction(period=3.0, actions=[spawn_robot_node])
 
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["diff_cont"],
     )
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=spawn_robot_node,
+            on_start=[diff_drive_spawner],
+        )
+    )
 
     joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_broad"],
+    )
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=spawn_robot_node,
+            on_start=[joint_broad_spawner],
+        )
     )
 
     twist_mux_params = os.path.join(gazebo_pkg, 'config', 'twist_mux.yaml')
@@ -90,8 +98,8 @@ def generate_launch_description():
             gazebo_launch,
             rviz_node,
             spawn_robot_node,
-            diff_drive_spawner,
-            joint_broad_spawner,
+            delayed_diff_drive_spawner,
+            delayed_joint_broad_spawner,
             twist_mux_node,
         ]
     )
